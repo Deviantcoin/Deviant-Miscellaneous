6f00b126fbf5ac6d89e41c482433ec5386b3d67e
ScaMar <scamar@gmail.com>
2018-10-04 01:01:45 +0200

    Update masternode_tor_setup.sh

--- linux/masternode_tor_setup.sh
+++ linux/masternode_tor_setup.sh
@@ -431,8 +431,8 @@ if [[ "$another" != "y" ]]
    fi
    if [[ "$REBOOTSYS" == "n" && -f /var/run/reboot-required ]]
    then echo -e "${RED}Keep in mind, this server still need a reboot${NC}"
-   echo "Good bye!"
    fi
+   echo "Good bye!"
   else setup_node
 fi
 }
